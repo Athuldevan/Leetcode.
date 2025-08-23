63df2d1a6421bc73f175272297138cb8f8ccff58
Athuldevan <108321451+Athuldevan@users.noreply.github.com>
2025-08-23 19:54:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.ts
+++ 0125-valid-palindrome/0125-valid-palindrome.ts
@@ -1,26 +1,20 @@
 function isPalindrome(s: string): boolean {
     if (!s) return true;
-
-    // Clean string: remove non-alphanumeric & lowercase
     const str = s.replace(/[^a-z0-9]/gi, "").toLowerCase();
-    console.log("cleaned:", str);
 
     let left = 0;
     let right = str.length - 1;
 
     while (left < right) {
         if (str[left] !== str[right]) {
-            return false; // mismatch found
+            return false
+        } else {
+            left++;
+            right++;
         }
-        left++;
-        right--;
+
     }
 
-    return true; // no mismatches found
-}
+    return true;
 
-// Test cases
-console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
-console.log(isPalindrome("racecar")); // true
-console.log(isPalindrome("hello"));   // false
-console.log(isPalindrome(".,,"));     // true (empty after cleaning)
+};
