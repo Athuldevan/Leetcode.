830f95f82d4a86f546569717e83cc01cd49a67d9
Athul <108321451+Athuldevan@users.noreply.github.com>
2025-10-30 21:35:12 +0530

    Time: 2 ms (78.18%), Space: 63.9 MB (26.15%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+    let maxArea = 0;
+
+    let left = 0;
+    let right = height.length - 1;
+    while (left < right) {
+        const length = right - left;
+        const width = Math.min(height[left], height[right]);
+        const area = length * width;
+        if (area > maxArea) {
+            maxArea = area;
+        }
+        if (height[left] > height[right]) {
+            right--
+
+        } else {
+            left++
+        }
+    }
+
+    return maxArea
+};
