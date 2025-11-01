ac97919190ef72c3ae0f2e22e3de78233798ca75
Athul <108321451+Athuldevan@users.noreply.github.com>
2025-11-01 17:28:17 +0530

    Time: 14 ms (66.6%), Space: 57.4 MB (42.61%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function (nums, target) {
+    nums.sort((a, b) => a - b);
+    let closest = Infinity;
+    let minDiff = Infinity;
+    for (let i = 0; i < nums.length - 2; i++) {
+        let left = i + 1;
+        let right = nums.length - 1;
+        while (left < right) {
+            let sum = nums[i] + nums[left] + nums[right];
+            let currDiff = Math.abs(sum - target);
+            if (currDiff < minDiff) {
+                minDiff = currDiff;
+                closest = sum;
+            } else if (sum < target) {
+                left++
+            } else {
+               right--
+            }
+
+        }
+    }
+    return closest;
+};
