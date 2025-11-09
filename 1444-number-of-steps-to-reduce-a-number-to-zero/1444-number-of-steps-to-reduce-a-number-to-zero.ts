7eb1e81e2ce59172d48c61c5508739947b238a70
Athul <108321451+Athuldevan@users.noreply.github.com>
2025-11-09 09:03:23 +0530

    Time: 0 ms (100%), Space: 54.5 MB (79.08%) - LeetHub

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.ts
@@ -0,0 +1,9 @@
+function numberOfSteps(num: number): number {
+    if (num === 0) return 0;
+
+    if (num % 2 === 0) {
+        return 1 + numberOfSteps(num / 2)
+    } else {
+        return 1 + numberOfSteps(num - 1)
+    }
+};
